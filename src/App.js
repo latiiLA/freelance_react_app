6ace8356c9a64196422e889c8a6ab7b756993b07
Lata <amelatbay@gmail.com>
2024-03-12 13:53:58 +0300

    Project is setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.svg";
+import "./App.css";
+
+// imported libraries
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// imported components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// imported pages
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/header" component={Header} />
+        <Route path="/footer" component={Footer} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+    // <div className="App">
+    //   Hello
+    //   <img src={logo} alt="alt prop" />
+    // </div>
   );
 }
 
