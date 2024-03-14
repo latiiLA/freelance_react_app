77b98dabc8ca7a6ecd31dde801446f0f2de4e2bb
Lata <amelatbay@gmail.com>
2024-03-14 11:03:43 +0300

    Home page implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+//Material Ui imports
+import { Box } from "@mui/material";
+
+//Component Imports
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Homebody from "../components/Homebody";
+import Howitworks from "../components/Howitworks";
+import Categories from "../components/Categories";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <Header />
+      <Homebody />
+      <Howitworks />
+      <Categories />
+      <Footer />
+    </Box>
+  );
 };
 
 export default Home;
