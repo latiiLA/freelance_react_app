da4b962085d01804338b73b67825cb2e02a69307
Lata <amelatbay@gmail.com>
2024-03-15 16:39:47 +0300

    employer profile page created

---
+++ src/pages/EmployerProfile.js
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const EmployerProfile = () => {
+  return (
+    <Box>
+      <Box>
+        <Box>
+          <Box>
+            <Box>Post</Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default EmployerProfile;
