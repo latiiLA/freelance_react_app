858930a196857871a047d468223b51f250d4e925
Lata <amelatbay@gmail.com>
2024-03-15 16:39:23 +0300

    Routing added

--- src/App.js
+++ src/App.js
@@ -10,12 +10,17 @@ import Footer from "./components/Footer";
 import Homebody from "./components/Homebody";
 import Howitworks from "./components/Howitworks";
 import Categories from "./components/Categories";
+import JobCard from "./components/JobCard";
 
 // imported pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import BrowseJobs from "./pages/BrowseJobs";
+import PostJobs from "./pages/PostJobs";
+import EmployerProfile from "./pages/EmployerProfile";
+import FreelancerProfile from "./pages/FreelancerProfile";
 
 function App() {
   return (
@@ -23,14 +28,19 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/browse" element={<BrowseJobs />} />
+        <Route path="/post" element={<PostJobs />} />
+        <Route path="/emppro" element={<EmployerProfile />} />
+        <Route path="/frepro" element={<FreelancerProfile />} />
 
         <Route path="/header" element={<Header />} />
         <Route path="/footer" element={<Footer />} />
         <Route path="/homebody" element={<Homebody />} />
         <Route path="/howitworks" element={<Howitworks />} />
         <Route path="/categories" element={<Categories />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/card" element={<JobCard />} />
       </Routes>
     </Router>
   );
