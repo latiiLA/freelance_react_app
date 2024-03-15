5c782bdcbf4cc51135d4e8474f1891e8e350662c
Lata <amelatbay@gmail.com>
2024-03-15 16:40:01 +0300

    freelancer profile page created

---
+++ src/pages/FreelancerProfile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const FreelancerProfile = () => {
+  return (
+    <Box>
+      <Box>
+        <Box>
+          <Box>
+            <Box>Post</Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default FreelancerProfile;
